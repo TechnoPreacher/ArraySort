c2cd31165b411a27a3425d261042ecc7a00a00ff
TechnoPreacher <nik.nik.sulima@gmail.com>
2022-08-30 13:03:51 +0300

    advanced

--- index.php
+++ index.php
@@ -6,43 +6,65 @@
  */
 
 
-function blob(array $array, bool $isASC = true): array
+function advancedBlob(array &$arrayToSort, bool $isASC = true): bool
 {
-    $sortedArray = $array;
-    for ($i = 1; $i <= sizeof($sortedArray); $i++) {
-        for ($k = 0; $k <= (sizeof($sortedArray) - 1 - $i); $k++) {
-            if ($sortedArray[$k] > $sortedArray[$k + 1]) {
-                $buffer = $sortedArray[$k + 1];
-                $sortedArray[$k + 1] = $sortedArray[$k];
-                $sortedArray[$k] = $buffer;
+    foreach ($arrayToSort as $v) {
+        if (!is_numeric($v)) {
+            return false;
+        }
+    }
+
+    for ($i = 1; $i <= sizeof($arrayToSort); $i++) {
+        for ($k = 0; $k <= (sizeof($arrayToSort) - 1 - $i); $k++) {
+            if ($arrayToSort[$k] > $arrayToSort[$k + 1]) {
+                list($arrayToSort[$k],$arrayToSort[$k + 1])=[$arrayToSort[$k + 1],$arrayToSort[$k]];
             }
         }
     }
 
     if (!$isASC) {
         $buffer = [];
-        for ($i = 1; $i <= sizeof($sortedArray); $i++) {
-            $buffer[] = $sortedArray[sizeof($sortedArray) - $i];
+        for ($i = 1; $i <= sizeof($arrayToSort); $i++) {
+            $buffer[] = $arrayToSort[sizeof($arrayToSort) - $i];
         }
-        $sortedArray = $buffer;
+        $arrayToSort = $buffer;
     }
-    return $sortedArray;
+    return true;
 }
+
 
 $array1 = [1, 2, 10, 3, 15, 48, 100, 92, 7, 10];
 $array2 = [23, 32, 44, 55, 0, 18];
+$array3 = ["ss", 32, 44, 55, 0, 18];
 
 echo "ARRAYS" . PHP_EOL;
-echo json_encode($array1) . PHP_EOL;//return [1,2,3,7,10,10,15,48,92,100]
-echo json_encode($array2) . PHP_EOL;//return [0,18,23,32,44,55]
-
+echo json_encode($array1) . PHP_EOL;
+echo json_encode($array2) . PHP_EOL;
+echo json_encode($array3) . PHP_EOL;
 
 echo PHP_EOL . "ASC" . PHP_EOL;
-echo json_encode(blob($array1)) . PHP_EOL;//return [1,2,3,7,10,10,15,48,92,100]
-echo json_encode(blob($array2)) . PHP_EOL;//return [0,18,23,32,44,55]
+
+echo (advancedBlob($array1) ? 'could convert' : 'could not convert!').' ';
+echo json_encode($array1) . PHP_EOL;
+
+echo (advancedBlob($array2) ? 'could convert' : 'could not convert!').' ';
+echo json_encode($array2) . PHP_EOL;
+
+echo (advancedBlob($array3) ? 'could convert' : 'could not convert!').' ';
+echo json_encode($array3) . PHP_EOL;
+
 
 echo PHP_EOL . "DESC" . PHP_EOL;
-echo json_encode(blob($array1, false)) . PHP_EOL;//return [100,92,48,15,10,10,7,3,2,1]
-echo json_encode(blob($array2, false)) . PHP_EOL;//return [55,44,32,23,18,0]
+
+echo (advancedBlob($array1,false) ? 'could convert' : 'could not convert!').' ';
+echo json_encode($array1) . PHP_EOL;//return [1,2,3,7,10,10,15,48,92,100]
+
+echo (advancedBlob($array2,false) ? 'could convert' : 'could not convert!').' ';
+echo json_encode($array2) . PHP_EOL;
+
+echo (advancedBlob($array3,false) ? 'could convert' : 'could not convert!').' ';
+echo json_encode($array3) . PHP_EOL;
+
+
 
 
